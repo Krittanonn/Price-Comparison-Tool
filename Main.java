a358701a06c0d30c0b0d2e137c24f6914c8db7e2
mss_ktn <155761604+Krittanonn@users.noreply.github.com>
2025-04-04 00:15:41 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+import src.PriceComparisonTool;
+
+public class Main {
+    public static void main(String[] args) {
+        PriceComparisonTool.main(args);  // Run the main method of the PriceComparisonTool class
+    }
+}
